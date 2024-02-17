e3e3020881e4c4d1115a84cd6c42fedbd2ff84c1
ahmedherhesh <ahmedherhesh3@gmail.com>
2024-02-17 17:58:10 +0200

    some work

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 @section('content')
     <x-nav />
-    <h3>My Google Maps Demo</h3>
+    <h3 class="text-center">Google Maps Does not Working</h3>
     <!--The div element for the map -->
     <div id="map"></div>
 
@@ -11,37 +11,6 @@
 @endsection
 
 @section('scripts')
-    <script>
-        (g => {
-            var h, a, k, p = "The Google Maps JavaScript API",
-                c = "google",
-                l = "importLibrary",
-                q = "__ib__",
-                m = document,
-                b = window;
-            b = b[c] || (b[c] = {});
-            var d = b.maps || (b.maps = {}),
-                r = new Set,
-                e = new URLSearchParams,
-                u = () => h || (h = new Promise(async (f, n) => {
-                    await (a = m.createElement("script"));
-                    e.set("libraries", [...r] + "");
-                    for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
-                    e.set("callback", c + ".maps." + q);
-                    a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
-                    d[q] = f;
-                    a.onerror = () => h = n(Error(p + " could not load."));
-                    a.nonce = m.querySelector("script[nonce]")?.nonce || "";
-                    m.head.append(a)
-                }));
-            d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() =>
-                d[l](f, ...n))
-        })
-        ({
-            key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg",
-            v: "weekly"
-        });
-    </script>
     <script>
         // Initialize and add the map
         let map;
@@ -75,8 +44,6 @@
                 title: "Uluru",
             });
         }
-
-        initMap();
     </script>
-
+    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCoU5YisydhoCp1KHBHgVcUVkvhis0yh0&loading=async&callback=initMap"></script>
 @endsection
